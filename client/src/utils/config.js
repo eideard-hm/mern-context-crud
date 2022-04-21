03c9371b0265b87a1b89648aab60b5e961a2796b
StivenLeon <stiven.leon@samtel.co>
2022-04-21 11:03:52 -0500

    Finished project

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,2 +1,2 @@
-export const POST_URL = 'http://localhost:4000/api/posts'
+export const POST_URL = 'https://mern-posts-crud.herokuapp.com/api/posts'
 export const NO_IMAGE_AVAILABLE = 'https://res.cloudinary.com/dga4gd7ig/image/upload/v1649277948/posts/no-image-available.png'
